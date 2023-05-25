5015b10b96f3de90caaa5b4aef72604acc8e2203
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-05-25 17:00:39 -0300

    se hizo la lógica de agregar un color al localstore

--- src/components/FormColor.jsx
+++ src/components/FormColor.jsx
@@ -1,7 +1,29 @@
 import { Card, Form, Button } from 'react-bootstrap';
 import ListColor from './ListColor/ListColor';
+import { useState, useEffect } from 'react';
 
 const FormColor = () => {
+  let colorLocalStorage =
+    JSON.parse(localStorage.getItem('listaColores')) || [];
+  const [color, setColor] = useState('');
+  const [listaColores, setListaColores] = useState(colorLocalStorage);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    agregarColor(color);
+  };
+
+  const agregarColor = (color) => {
+    //se agrega nueva tarea a la lista de tareas
+    setListaColores([...listaColores, color]);
+    //limpiar el input Tarea
+    setColor('');
+  };
+
+  useEffect(() => {
+    localStorage.setItem('listaColores', JSON.stringify(listaColores));
+  }, [listaColores]);
+
   return (
     <>
       {/* Agrego una card */}
@@ -11,12 +33,14 @@ const FormColor = () => {
             Administrar Colores
           </Card.Title>
           {/* Agrego un formulario  */}
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="inputColor">
               <Form.Label className="display-6">Ingrese un color:</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Ingrese un color como por ej: Blue"
+                value={color}
+                onChange={(event) => setColor(event.target.value)}
               />
             </Form.Group>
 
